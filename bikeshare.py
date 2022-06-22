f0b014017fb197bd229e56e17a58a87b75e2045d
Mohamed <msalodosari8@gmail.com>
2022-06-22 11:16:41 +0300

    refactor: function get_filters header comment has been deleted since it is not needed at bikeshare.py file

--- bikeshare.py
+++ bikeshare.py
@@ -7,14 +7,7 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'washington': 'washington.csv' }
 
 def get_filters():
-    """
-    Asks user to specify a city, month, and day to analyze.
-
-    Returns:
-        (str) city - name of the city to analyze
-        (str) month - name of the month to filter by, or "all" to apply no month filter
-        (str) day - name of the day of week to filter by, or "all" to apply no day filter
-    """
+   
     print('\nHi.. Let\'s get your specific inputs for the bikeshare data you want to analyze:')
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     while True:
